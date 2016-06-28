5b0f6454ad7367c9ef62bb3bc95be3a71e8d41da
Angel Olea <angelolea24@gmail.com>
2016-06-27 22:10:17 -0400

    finished with no validations

--- programming-foundations/lesson-2/mortgage_calculator.rb
+++ programming-foundations/lesson-2/mortgage_calculator.rb
@@ -1,1 +1,52 @@
+require 'pry'
 
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+
+def loan_duration_in_months(years)
+  years / 12
+end
+
+def monthly_interest_rate(apr)
+  (apr / 100) / 12
+end
+
+def number_of_payments(years)
+  years * 12
+end
+
+def monthly_payments(loan, apr, years)
+  mir = monthly_interest_rate(apr)
+  nop = number_of_payments(years)
+
+  payment = mir * (mir + 1)**nop / ((1 + mir)**nop - 1) * loan
+  payment.round(2)
+end
+
+loop do
+  prompt("Welcome to Mortgage Calculator")
+
+  prompt("What is your loan amount?")
+  principal = Kernel.gets().chomp().to_f
+
+  prompt("What is the APR?")
+  apr = Kernel.gets().chomp().to_f
+
+  prompt("What is the duration of the loan in years?")
+  duration = Kernel.gets().chomp().to_f
+
+  payment = monthly_payments(principal, apr, duration)
+
+  prompt("Your monthly payment is: $#{payment}")
+
+  prompt("Would you like to make another calculation?")
+  answer = Kernel.gets().chomp.downcase
+
+  break unless answer == 'y'
+
+  prompt("Thanks for using Mortgage Calculator!")
+  prompt("Have a great day!")
+
+end
